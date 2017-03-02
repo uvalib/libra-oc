ff429541664e70fc3591f4298ca5c602b26f9b18
Nestor Walker <naw4t@virginia.edu>
2017-03-02 17:46:40 -0500

    temporarily add inline queue adapter

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module LibraOc
 
     config.tinymce.install = :copy
 
+    # this is only temporary
+    config.active_job.queue_adapter = :inline
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
