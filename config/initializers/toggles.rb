920b5acb4073aed0151cab6f5271c953c7de8f6b
UVADave <dpg3k@virginia.edu>
2017-03-15 14:17:34 -0400

    Correct toggles implementation and initialization

--- config/initializers/toggles.rb
+++ config/initializers/toggles.rb
@@ -1,29 +1,21 @@
 require_dependency 'libraoc/toggles'
 
 #
-# If we add code to turn on or off certain features (sufia for example), configure them
-# here.
+# do we expose the idea of collections of works?
+# the default is true
+Toggles.config.expose_collections = false
+
 #
-Toggles.config do |config|
+# do we expose the idea of users following other users?
+# the default is true
+Toggles.config.expose_follows = false
 
-  #
-  # do we expose the idea of collections of works?
-  # the default is true
-  config.expose_collections = false
+#
+# do we expose the idea of highlighted works?
+# the default is true
+Toggles.config.expose_highlights = false
 
-  #
-  # do we expose the idea of users following other users?
-  # the default is true
-  config.expose_follows = false
-
-  #
-  # do we expose the idea of highlighted works?
-  # the default is true
-  config.expose_highlights = false
-
-  #
-  # do we expose the idea of ownership transfer?
-  # the default is true
-  config.expose_ownership_transfer = false
-
-end
+#
+# do we expose the idea of ownership transfer?
+# the default is true
+Toggles.config.expose_ownership_transfer = false
