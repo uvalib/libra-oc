1cbc8873c55ad5492ef158e8ca2322f51d977a87
UVADave <dpg3k@virginia.edu>
2017-03-15 14:29:28 -0400

    And again...

--- lib/libraoc/toggles.rb
+++ lib/libraoc/toggles.rb
@@ -10,22 +10,29 @@ module Toggles
 
     attr_writer :expose_collections
     def expose_collections
-      @expose_collections ||= true
+      @expose_collections
     end
 
     attr_writer :expose_follows
     def expose_follows
-      @expose_follows ||= true
+      @expose_follows
     end
 
     attr_writer :expose_highlights
     def expose_highlights
-      @expose_highlights ||= true
+      @expose_highlights
     end
 
     attr_writer :expose_ownership_transfer
     def expose_ownership_transfer
-      @expose_ownership_transfer ||= true
+      @expose_ownership_transfer
+    end
+
+    def initialize
+      @expose_collections = true
+      @expose_follows = true
+      @expose_highlights = true
+      @expose_ownership_transfer = true
     end
 
   end
