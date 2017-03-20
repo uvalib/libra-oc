af1a2a2c2968ccb81ab2ed3351b6a4f0521682e2
Nestor Walker <naw4t@virginia.edu>
2017-03-20 16:26:53 -0400

    catch and still show non-json person fields

--- app/renderers/person_attribute_renderer.rb
+++ app/renderers/person_attribute_renderer.rb
@@ -1,6 +1,12 @@
 class PersonAttributeRenderer < CurationConcerns::Renderers::AttributeRenderer
   def attribute_value_to_html person_json
-    person = JSON.parse(person_json)
+    person = {}
+    begin
+      person = JSON.parse(person_json)
+    rescue JSON::ParserError => e
+      person = person_json
+    end
+
     tags = ''
     %w(first_name last_name department institution).map do |key|
       tags << "#{key.titleize}: #{person[key]}<br/>"
