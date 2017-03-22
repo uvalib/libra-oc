884a3cdd64e1426d688bbaec45ef62de7a158efd
UVADave <dpg3k@virginia.edu>
2017-03-22 10:06:50 -0400

    Add sidekiq initializer so we talk to the configured instance of redis

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,13 @@
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+rails_env = ENV['RAILS_ENV'] || 'development'
+
+config = YAML.load(ERB.new(IO.read(File.join(rails_root, 'config', 'redis.yml'))).result)[rails_env]
+config[:namespace] = "libra-oc-workerpool:#{rails_env}"
+
+Sidekiq.configure_server do |s|
+  s.redis = config
+end
+
+Sidekiq.configure_client do |s|
+  s.redis = config
+end
