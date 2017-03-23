2124cfe725e0dafd797279c13aaf0533eb5b4000
Nestor Walker <naw4t@virginia.edu>
2017-03-23 14:28:17 -0400

    LIBRAOC-112 consistent visibility labels

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -2,4 +2,27 @@ module SufiaHelper
   include ::BlacklightHelper
   include Sufia::BlacklightOverride
   include Sufia::SufiaHelperBehavior
+
+  def render_libra_visibility_link(document)
+    # Anchor must match with a tab in
+    # https://github.com/projecthydra/sufia/blob/master/app/views/curation_concerns/base/_guts4form.html.erb#L2
+    link_to render_visibility_label(document),
+      edit_polymorphic_path([main_app, document], anchor: "share"),
+      id: "permission_" + document.id, class: "visibility-link"
+  end
+
+  private
+
+  def render_visibility_label(document)
+    if document.registered?
+      content_tag :span, t('curation_concerns.visibility.authenticated.label_html'),
+        class: "label label-warning", title: institution_name
+    elsif document.public?
+      content_tag :span, t('sufia.visibility.open'), class: "label label-success",
+        title: t('sufia.visibility.open_title_attr')
+    else
+      content_tag :span, t('sufia.visibility.private'), class: "label label-danger",
+        title: t('sufia.visibility.private_title_attr')
+    end
+  end
 end
--- app/presenters/libra_work_presenter.rb
+++ app/presenters/libra_work_presenter.rb
@@ -1,4 +1,5 @@
 class LibraWorkPresenter < Sufia::WorkShowPresenter
+  include ActionView::Helpers::TagHelper
 
   # add our custom fields to the presenter
   delegate :authors,
@@ -17,5 +18,54 @@ class LibraWorkPresenter < Sufia::WorkShowPresenter
 
      to: :solr_document
 
+  def libra_permission_badge
+    content_tag(:span, link_title, title: link_title, class: "label #{dom_label_class}")
+  end
+
+  private
+  def dom_label_class
+    if open_access_with_embargo?
+      'label-warning'
+    elsif open_access?
+      'label-success'
+    elsif registered?
+      'label-warning'
+    else
+      'label-danger'
+    end
+  end
+
+  def link_title
+    if open_access_with_embargo?
+      'Open Access with Embargo'
+    elsif open_access?
+      'Open Access'
+    elsif registered?
+      I18n.translate('curation_concerns.institution_name')
+    else
+      'Private'
+    end
+  end
+
+  def open_access_with_embargo?
+    if @open_access_with_embargo.nil?
+      @open_access_with_embargo = open_access? && embargo?
+    end
+    @open_access_with_embargo
+  end
+
+  def open_access?
+    @open_access = @solr_document.visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC if @open_access.nil?
+    @open_access
+  end
+
+  def registered?
+    @registered = @solr_document.visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED if @registered.nil?
+    @registered
+  end
+
+  def embargo?
+    @solr_document.embargo_release_date.present?
+  end
 
 end
