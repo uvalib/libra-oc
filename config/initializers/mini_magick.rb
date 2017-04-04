9c85d7d302f64a275d237b393df23207dc3b4475
Nestor Walker <naw4t@virginia.edu>
2017-04-04 10:49:45 -0400

    attachment rake task

--- config/initializers/mini_magick.rb
+++ config/initializers/mini_magick.rb
@@ -2,4 +2,9 @@ require 'mini_magick'
 
 MiniMagick.configure do |config|
   config.shell_api = "posix-spawn"
+  config.timeout = 1800
+  config.validate_on_create = false
+  config.validate_on_write = false
+  config.logger.level = Logger::DEBUG
+
 end
