4aa30ed9d4a801a88102b3012a771a51a65cfd91
UVADave <dpg3k@virginia.edu>
2017-04-20 08:20:17 -0400

    LIBRAOC-150: include rights name as well as link in the email

--- app/mailers/work_mailer.rb
+++ app/mailers/work_mailer.rb
@@ -6,7 +6,8 @@ class WorkMailer < ActionMailer::Base
 	def work_submitted( work, to, from )
 		@recipient = User.displayname_from_email( to )
     @visibility = work.is_publicly_visible? ? 'public' : 'UVa-only'
-		@rights = work.rights.join(' ')
+		@rights_link = work.rights.first
+    @rights_name = RightsService.label( @rights_link )
 		@doi_url = work.doi_url
 		subject = 'Work successfully deposited to libra-oc'
 		logger.info "Sending email (successful deposit); to: #{to} (#{@recipient}), from: #{from}, subject: #{subject}"
