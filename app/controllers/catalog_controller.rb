e1a48f1d4aca01897f42bc5ac2d696e864d985b4
Nestor Walker <naw4t@virginia.edu>
2017-05-08 15:59:05 -0400

    LIBRAOC-215 facetable authors and contributors

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -45,8 +45,8 @@ class CatalogController < ApplicationController
     #   The ordering of the field names is the order of the display
     #config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
-    config.add_facet_field solr_name("authors", :facetable), label: "Author", limit: 5
-    config.add_facet_field solr_name("contributors", :facetable), label: "Contributor", limit: 5
+    config.add_facet_field solr_name("authors_display", :facetable), label: "Author", limit: 5
+    config.add_facet_field solr_name("contributors_display", :facetable), label: "Contributor", limit: 5
     config.add_facet_field solr_name("keyword", :facetable), label: "Keyword", limit: 5
     config.add_facet_field solr_name("subject", :facetable), label: "Subject", limit: 5
     config.add_facet_field solr_name("language", :facetable), label: "Language", limit: 5
--- app/indexers/libra_oc_indexer.rb
+++ app/indexers/libra_oc_indexer.rb
@@ -3,7 +3,9 @@ class LibraOcIndexer < CurationConcerns::WorkIndexer
   def generate_solr_document
     super.tap do |solr_doc|
       solr_doc[Solrizer.solr_name('authors')] = object.authors
+      solr_doc[Solrizer.solr_name('authors_display', :facetable)] = object.authors.map &:to_display
       solr_doc[Solrizer.solr_name('contributors')] = object.contributors
+      solr_doc[Solrizer.solr_name('contributors_display', :facetable)] = object.contributors.map &:to_display
     end
   end
 end
