f48ccbc8829851b06d3cfa8b62c2b8cb0a9359d6
UVADave <dpg3k@virginia.edu>
2017-05-25 15:36:32 -0400

    LIBRAOC-277: no audit for removal of file on a provate work

--- app/jobs/fileset_removed_audit_job.rb
+++ app/jobs/fileset_removed_audit_job.rb
@@ -4,7 +4,10 @@ class FilesetRemovedAuditJob < ActiveJob::Base
 
     user_id = user.computing_id
     user_id = 'none' if user_id.blank?
-    Audit.audit( fileset.in_works.first.id, user_id, 'files', fileset.label, '' )
+    work = fileset.in_works.first
+    return if work.nil?
+    return if work.is_private?
+    Audit.audit( work.id, user_id, 'files', fileset.label, '' )
   end
 
 end
