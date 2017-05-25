f1fb66cd5811d2602236292b0d02e17583fa617b
UVADave <dpg3k@virginia.edu>
2017-05-25 11:44:31 -0400

    LIBRAOC-283: proposed fix by defining sufia config appropriatly

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -98,8 +98,9 @@ Sufia.config do |config|
   # config.banner_image = 'https://cloud.githubusercontent.com/assets/92044/18370978/88ecac20-75f6-11e6-8399-6536640ef695.jpg'
 
   # Temporary path to hold uploads before they are ingested into FCrepo.
-  # This must be a lambda that returns a Pathname
+  # These must be lambdas that return a Pathname. Can be configured separately
   config.upload_path = ->() { Rails.root + 'hostfs' + 'uploads' + 'tmp' }
+  config.cache_path = ->() { Rails.root + 'hostfs' + 'uploads' + 'cache' }
 
   # Location on local file system where derivatives will be stored.
   # If you use a multi-server architecture, this MUST be a shared volume.
