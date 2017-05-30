b9117728d1aed5b29cd614310accb10d0595ad4b
Nestor Walker <naw4t@virginia.edu>
2017-05-30 17:21:52 -0400

    LIBRAOC-272 better single use link error page

---
+++ app/controllers/curation_concerns/single_use_links_viewer_controller.rb
@@ -0,0 +1,33 @@
+module CurationConcerns
+  class SingleUseLinksViewerController < ApplicationController
+    include CurationConcerns::SingleUseLinksViewerControllerBehavior
+
+
+
+
+    class Ability
+      include CanCan::Ability
+
+      attr_reader :single_use_link
+
+      def initialize(user, single_use_link)
+        @user = user || ::User.new
+
+        @single_use_link = single_use_link
+
+        can :read, [ActiveFedora::Base, SolrDocument] do |obj|
+          single_use_link.valid? && single_use_link.itemId == obj.id && single_use_link.destroy!
+        end if single_use_link
+      end
+    end
+
+
+    protected
+
+    def render_single_use_error(exception)
+      logger.error("Rendering PAGE due to exception: #{exception.inspect} - #{exception.backtrace if exception.respond_to? :backtrace}")
+      render 'single_use_error',layout: 'public_view', status: 404
+    end
+
+  end
+end
