52b2dc8597af1c9379d1029e94091a525bd77582
Nestor Walker <naw4t@virginia.edu>
2017-05-30 15:07:22 -0400

    LIBRAOC-292 save orcid response, show error messages

---
+++ db/migrate/20170530162116_add_orcid_auth_to_users.rb
@@ -0,0 +1,10 @@
+class AddOrcidAuthToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :orcid_access_token, :string
+    add_column :users, :orcid_refresh_token, :string
+    add_column :users, :orcid_scope, :string
+    add_column :users, :orcid_expires_at, :datetime
+    add_column :users, :orcid_linked_at, :datetime
+
+  end
+end
