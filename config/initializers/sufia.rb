be377b7be1111ec3348fa7fc35fc168fc570a29e
UVADave <dpg3k@virginia.edu>
2017-06-02 09:17:13 -0400

    LIBRAOC-283: Try to use the /tmp filesystem

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -100,7 +100,8 @@ Sufia.config do |config|
   # Temporary path to hold uploads before they are ingested into FCrepo.
   # These must be lambdas that return a Pathname. Can be configured separately
   config.upload_path = ->() { Rails.root + 'hostfs' + 'uploads' + 'originals' }
-  config.cache_path = ->() { Rails.root + 'hostfs' + 'uploads' + 'cache' }
+  #config.cache_path = ->() { Rails.root + 'hostfs' + 'uploads' + 'cache' }
+  config.cache_path = ->() { '/tmp' }
 
   # Location on local file system where derivatives will be stored.
   # If you use a multi-server architecture, this MUST be a shared volume.
