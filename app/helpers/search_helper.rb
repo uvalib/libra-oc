cf737f24d5fda72b77b05e6072fd5a1b0041a3ce
UVADave <dpg3k@virginia.edu>
2017-06-07 10:59:32 -0400

    LIBRAOC-321: change search box placeholder text

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,4 +7,12 @@ module SearchHelper
       sufia.dashboard_works_path
     end
   end
+
+  def search_scope_placeholder_text
+    if current_user && current_user.admin?
+      return t("sufia.search.form.q.placeholder_all_scope")
+    else
+      return t("sufia.search.form.q.placeholder_my_scope")
+    end
+  end
 end
