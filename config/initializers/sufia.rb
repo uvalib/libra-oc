3beba3fb3753a6d0215144a1e6a7d8b78751d493
UVADave <dpg3k@virginia.edu>
2017-11-08 09:41:35 -0500

    Move to fits-1.2.0

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -54,7 +54,7 @@ Sufia.config do |config|
 
   # Specify the path to the file characterization tool:
   # config.fits_path = "fits.sh"
-  config.fits_path = "#{Rails.application.root}/tools/fits-1.0.5/fits.sh"
+  config.fits_path = "#{Rails.application.root}/tools/fits-1.2.0/fits.sh"
 
   # Specify the path to the file derivatives creation tool:
   # config.libreoffice_path = "soffice"
