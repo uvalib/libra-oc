df389f675d8e13660cdafdf84007c658c17d8d1b
Dave Goldstein <dpg3k@virginia.edu>
2020-05-21 08:27:46 -0400

    Log to standard out, better for the AWS environment

--- scripts/update_viruscheck_signatures.sh
+++ scripts/update_viruscheck_signatures.sh
@@ -6,35 +6,34 @@
 DIR=$(dirname $0)
 . $DIR/common.sh
 
-# set the appropriate logger
-export NAME=$(basename $0 .sh)
-export LOGGER=$(logger_name "$NAME.log")
-
 # the time we want the action to occur
 # this is specified in localtime
 export ACTION_TIME="01:00"
 export ACTION_TIMEZONE="America/New_York"
 
 # helpful message...
-logit "Virus check signature refresher starting up..."
+logit "INFO: Virus check signature refresher starting up..."
+
+# sleep for a bit so we do not do a refresh immediatly
+sleep 60
 
 # forever...
 while true; do
 
    # starting message
-   logit "Beginning virus check signature refresh sequence"
+   logit "INFO: Beginning virus check signature refresh sequence"
 
-   rake libraoc:antivirus:refresh >> $LOGGER 2>&1
+   rake libraoc:antivirus:refresh
    res=$?
 
    # ending message
-   logit "Virus check signature refresh completes with status: $res"
+   logit "INFO: Virus check signature refresh completes with status: $res"
 
    # sleep for another minute
    sleep 60
 
    # sleeping message...
-   logit "Sleeping until $ACTION_TIME $ACTION_TIMEZONE..."
+   logit "INFO: Sleeping until $ACTION_TIME $ACTION_TIMEZONE..."
    sleep_until $ACTION_TIME $ACTION_TIMEZONE
 
 done
