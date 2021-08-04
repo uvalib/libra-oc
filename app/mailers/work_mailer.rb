f0948fdfbafa8241a7650808aacbf21cf4a6a0d1
Nestor Walker <naw4t@virginia.edu>
2021-08-04 14:21:02 -0400

    LIBRAOC-613 add embargo info to submition email

--- app/mailers/work_mailer.rb
+++ app/mailers/work_mailer.rb
@@ -1,4 +1,5 @@
 class WorkMailer < ActionMailer::Base
+	add_template_helper(PublicViewHelper)
 
 	#
 	# user has just submitted a work, send them a success email
@@ -7,6 +8,8 @@ class WorkMailer < ActionMailer::Base
 		@recipient = User.displayname_from_email( to )
 		@title = work.title.first
     @visibility = work.is_publicly_visible? ? 'public' : 'UVa-only'
+		@embargo_release_date = work.embargo_release_date
+		@visibility_after_embargo = work.visibility_after_embargo
     @rights_name = RightsService.label( work.rights.first )
 		@doi_url = work.doi_url
 		subject = 'Work successfully deposited to Libra'
